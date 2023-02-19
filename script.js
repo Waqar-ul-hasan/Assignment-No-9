e69fbd2b36da7a9e63f73df8121838e90ada650d
Hafiz Haris <harismubeen74@gmail.com>
2023-02-19 22:53:52 +0500

    Assignment No 9

---
+++ script.js
@@ -0,0 +1,103 @@
+let 
+    myVar = 5;
+console.log(myVar);
+
+
+let
+    myString = "Hello, world!";
+alert(myString);
+
+
+let 
+    num1 = 5;
+let 
+    num2 = 10;
+let  
+    userNum = parseInt(prompt("Please enter a number:"));
+let 
+    sum = num1 + num2 + userNum;
+console.log(sum);
+
+
+let 
+    myBoolean = true;
+if  
+   (myBoolean) {
+   console.log("It's true!");
+} 
+else {
+   console.log("It's false!");
+}
+
+
+let 
+    number = 7;
+if 
+   (number % 3 === 2) {
+   console.log("even");
+}
+else {
+   console.log("odd");
+}
+
+
+let  
+   str1 = "Waqar-Ul";
+let 
+   str2 = "-Hassan";
+let 
+   result = str1 + str2;
+console.log(result);
+
+
+let
+   number1 = 2;
+let
+   nummber2 = 3;
+let
+   result2 = num1 * num2;
+console.log(result2);
+
+
+let input = prompt("Enter your password:");
+if (input === "password") {
+   alert("Access pass");
+}
+else {
+   alert("Access fail");
+}
+
+
+let 
+    numb1 = parseFloat(prompt("Enter first number:"));
+let 
+    numb2 = parseFloat(prompt("Enter second number:"));
+let 
+    numb3 = parseFloat(prompt("Enter third number:"));
+if 
+   (numb1 >= numb2 && numb1 >= numb3) {
+   console.log(numb1);
+} 
+else if 
+   (numb2 >= numb1 && numb2 >= numb3) {
+   console.log(numb2);
+} else {
+   console.log(numb3);
+}
+
+
+let
+    string1 = prompt("Enter string 1:"); 
+let  
+    string2 = prompt("Enter string 2:"); 
+
+if 
+    (string1 === string2) {
+    alert("The strings are equal"); 
+}
+else {
+   alert("The strings are not equal"); 
+}
+
+
+
